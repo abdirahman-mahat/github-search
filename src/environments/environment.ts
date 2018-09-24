5204288f2e43a5b7b75fba52461f336c134871b3
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-09-24 14:45:58 +0300

    add styles to body

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiKEY:"a9dd3bf1dbfcc066749d59eceea76343f813b04b",
 };
 
