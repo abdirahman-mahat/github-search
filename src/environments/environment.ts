61f9aa35d61603099901f671248a64792d11925e
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-09-24 15:02:05 +0300

    add all changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiKEY:"a9dd3bf1dbfcc066749d59eceea76343f813b04b",
 };
 
