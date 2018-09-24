2e3521b9af55afb50cfcf333f675b585d2797bb8
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-09-24 18:31:44 +0300

    add the highlight directive

--- src/app/search-request.service.ts
+++ src/app/search-request.service.ts
@@ -33,7 +33,7 @@ export class SearchRequestService {
         }
 
         const promise = new Promise((resolve) => {
-            this.http.get<ApiResponse>('https://api.github.com/users/' + searchName + '?access_token=' + environment.apiKEY).toPromise().then(getResponse => {
+            this.http.get<ApiResponse>('https://api.github.com/users/' + searchName + '?access_token=' + environment.apiKey).toPromise().then(getResponse => {
                 this.users.name = getResponse.name;
                 this.users.html_url = getResponse.html_url;
                 this.users.login = getResponse.login;
@@ -57,7 +57,7 @@ export class SearchRequestService {
         }
 
         const myPromise = new Promise((resolve, reject) => {
-            this.http.get<ApiResponse>('https://api.github.com/users/' + searchMe + '/repos?order=created&sort=asc?access_token=' + environment.apiKEY).toPromise().then(getRepoResponse => {
+            this.http.get<ApiResponse>('https://api.github.com/users/' + searchMe + '/repos?order=created&sort=asc?access_token=' + environment.apiKey).toPromise().then(getRepoResponse => {
                 this.newRepository = getRepoResponse;
                 resolve();
             }, error => {
@@ -74,7 +74,7 @@ export class SearchRequestService {
         }
 
         const promise = new Promise((resolve, reject) => {
-            this.http.get<ApiResponse>('https://api.github.com/search/repositories?q=' + searchName + ' &per_page=10 ' + environment.apiKEY).toPromise().then(getRepoResponse => {
+            this.http.get<ApiResponse>('https://api.github.com/search/repositories?q=' + searchName + ' &per_page=10 ' + environment.apiKey).toPromise().then(getRepoResponse => {
                 this.searchRepo = getRepoResponse.items;
 
                 resolve();
