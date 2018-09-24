04cbda91017e173727b9a3823a15f607cc4e9d23
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-09-24 08:49:30 +0300

    add highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
-
+import { Directive,HostListener,Input,ElementRef, OnInit } from '@angular/core';
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-
-  constructor() { }
-
+ @Input("appHighlight") description:boolean;
+  constructor(private elem:ElementRef) {
 }
+  @HostListener('change') ngOnChanges(){
+    if(this.description){
+      this.elem.nativeElement.style.backgroundColor='#444';
+      this.elem.nativeElement.style.color='#000';
+    }
+    else{
+      this.elem.nativeElement.style.backgroundColor='#082845';
+      this.elem.nativeElement.style.color='#fff';
+    }
+  }
+    ngOnInit(){
+
+    }
+  }
